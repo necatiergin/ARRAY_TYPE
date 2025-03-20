cff9aba5ed408e616c318daf36b781c330dc0ed2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-03-20 14:27:48 +0300

    Create ar_09.cpp

---
+++ ar_09.cpp
@@ -0,0 +1,18 @@
+#include <iterator>
+
+template <typename T, std::size_t N>
+constexpr auto Size(const T(&)[N]) noexcept
+{
+	return N;
+}
+
+int main()
+{
+	int a1[10]{};
+	double a2[20]{};
+	int a3[5][10]{};
+
+	constexpr auto sz1 = std::size(a1);
+	constexpr auto sz2 = std::size(a2);
+	constexpr auto sz3 = std::size(a3);
+}
